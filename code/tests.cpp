c35cf935afcbcea57b03b339e89e5c9d210ccb97
aishpasham <112121940+aishpasham@users.noreply.github.com>
2023-10-30 22:46:45 -0500

    Add files via upload

--- code/tests.cpp
+++ code/tests.cpp
@@ -5,29 +5,41 @@
 #include <sstream>
 
 
-TEST_CASE("Password Strength Prediction Complex Alphanumeric", "[weight=5]")
-{
-    std::string dataset = "../../data/data.csv";
-    std::string password = "AVYq1lDE4MgAZfNt";
-    int out = predictStrength(dataset, password);
-    int expected = 5;
-    REQUIRE(expected == out);
-}
-
-TEST_CASE("Password Strength Prediction Simple Numeric", "[weight=5]")
-{
-    std::string dataset = "../../data/passwords.csv";
-    std::string password = "111111";
-    int out = predictStrength(dataset, password);
-    int expected = 1;
-    REQUIRE(expected == out);
-}
-
-TEST_CASE("Common Pattern Name", "[weight=5]")
+TEST_CASE("Find Repeats Numeric", "[weight=5]")
 {
     std::string dataset = "../../data/500-worst-passwords.txt";
-    std::string password = "joseph";
-    std::string out = commonPattern(dataset, password);
-    std::string expected = "Name";
-    REQUIRE(expected == out);
+    
+    std::vector<int> out = findRepeats(dataset, "123");
+
+    std::vector<int> expected = {0, 3};
+
+    REQUIRE(out.size() == expected.size())
+    
+    //95 is index of password where password "123123" occurs in dataset
+    REQUIRE(out.at(95) == expected);
+}
+
+TEST_CASE("Find Repeats Alphabetic", "[weight=5]")
+{
+    std::string dataset = "../../data/passwords.csv";
+    
+    std::vector<int> out = findRepeats(dataset, "aa");
+
+    std::vector<int> expected = {0, 2, 4};
+
+    REQUIRE(out.size() == expected.size())
+    
+    //105 is index of password where password "aaaaaa" occurs in dataset
+    REQUIRE(out.at(105) == expected);
+}
+
+TEST_CASE("Password Strength Prediction", "[weight=5]")
+{
+    std::string dataset = "../../data/passwords.csv";
+    
+    int out = predictStrength(dataset, "aaaaaa", "aa");
+
+    std::vector<int> expected = 1;
+    
+    REQUIRE(out == expected);
 }
